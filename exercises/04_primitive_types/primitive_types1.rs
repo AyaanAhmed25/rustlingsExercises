42b636017e7f2a4b8ff3051faccd6152a0a728e8
AyaanAhmed25 <ayaan.ahmed.25@icloud.com>
2024-11-26 21:48:33 -0500

    init

---
+++ exercises/04_primitive_types/primitive_types1.rs
@@ -0,0 +1,21 @@
+// Booleans (`bool`)
+
+fn main() {
+    let is_morning = true;
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    // TODO: Define a boolean variable with the name `is_evening` before the `if` statement below.
+    // The value of the variable should be the negation (opposite) of `is_morning`.
+    // let …
+    let mut is_evening: bool = false;
+    if is_morning == true {
+        is_evening = false;
+    } else {
+        is_evening == true;
+    }
+    if is_evening {
+        println!("Good evening!");
+    }
+}
